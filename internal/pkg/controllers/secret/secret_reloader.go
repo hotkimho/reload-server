0991af3aa18d7bd44e25137691041c00269e9fad
hotkimho <rlaghekd777@naver.com>
2024-09-22 23:27:44 +0900

    feat: 공용 함수로 분리된 기능 메소드로 개별 구현 - 스테이트풀셋, 데몬셋 리소스도 reload기능 추가

--- internal/pkg/controllers/secret/secret_reloader.go
+++ internal/pkg/controllers/secret/secret_reloader.go
@@ -2,6 +2,7 @@ package secret
 
 import (
 	"context"
+	"errors"
 	"reflect"
 	"time"
 
@@ -9,6 +10,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -20,6 +22,7 @@ import (
 	"github.com/go-logr/logr"
 
 	"github.com/hotkimho/reloader-server/project/internal/pkg/constants"
+	"github.com/hotkimho/reloader-server/project/internal/pkg/utils"
 )
 
 type SecretController struct {
@@ -45,6 +48,24 @@ func SetupSecretController(mgr ctrl.Manager) error {
 		})
 }
 
+func secretUpdateEventFilter(e event.UpdateEvent) bool {
+	oldObj, oldOk := e.ObjectOld.(*corev1.Secret)
+	newObj, newOk := e.ObjectNew.(*corev1.Secret)
+	if !oldOk || !newOk {
+		return false
+	}
+
+	if _, ok := newObj.GetLabels()[constants.ReloaderLabelKey]; !ok {
+		return false
+	}
+
+	if reflect.DeepEqual(oldObj.Data, newObj.Data) {
+		return false
+	}
+
+	return true
+}
+
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=secrets/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=core,resources=secrets/finalizers,verbs=update
@@ -55,7 +76,17 @@ func (r *SecretController) Reconcile(ctx context.Context, req reconcile.Request)
 		return reconcile.Result{}, client.IgnoreNotFound(err)
 	}
 
-	if err := r.reloadDeployments(ctx, s); err != nil {
+	nsdName := types.NamespacedName{
+		Namespace: s.Namespace,
+		Name:      s.GetLabels()[constants.ReloaderLabelKey],
+	}
+
+	rr, err := r.getReloadResource(ctx, nsdName)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if err := r.reloadResource(ctx, rr); err != nil {
 		return ctrl.Result{}, err
 	}
 
@@ -93,20 +124,87 @@ func (r *SecretController) reloadDeployments(ctx context.Context, secret *corev1
 	return nil
 }
 
-func secretUpdateEventFilter(e event.UpdateEvent) bool {
-	oldObj, oldOk := e.ObjectOld.(*corev1.Secret)
-	newObj, newOk := e.ObjectNew.(*corev1.Secret)
-	if !oldOk || !newOk {
-		return false
+func (r *SecretController) getReloadResource(ctx context.Context, namespacedName types.NamespacedName) (client.Object, error) {
+	if d, err := utils.GetDeployment(r.client, ctx, namespacedName); err == nil {
+		return d, nil
 	}
 
-	if _, ok := newObj.GetLabels()[constants.ReloaderLabelKey]; !ok {
-		return false
+	if ss, err := utils.GetStatefulSet(r.client, ctx, namespacedName); err == nil {
+		return ss, nil
 	}
 
-	if reflect.DeepEqual(oldObj.Data, newObj.Data) {
-		return false
+	if ds, err := utils.GetDaemonSet(r.client, ctx, namespacedName); err == nil {
+		return ds, nil
 	}
 
-	return true
+	return nil, errors.New("not found resource type")
+}
+
+func (r *SecretController) reloadResource(ctx context.Context, obj client.Object) error {
+	switch rr := obj.(type) {
+	case *appsv1.Deployment:
+		if err := r.reloadDeployment(ctx, rr); err != nil {
+			return err
+		}
+
+	case *appsv1.StatefulSet:
+		if err := r.reloadStatefulSet(ctx, rr); err != nil {
+			return err
+		}
+
+	case *appsv1.DaemonSet:
+		if err := r.reloadDaemonSet(ctx, rr); err != nil {
+			return err
+		}
+	}
+
+	return errors.New("not found resource type")
+}
+
+func (r *SecretController) reloadDeployment(ctx context.Context, d *appsv1.Deployment) error {
+	if d.Spec.Template.Annotations == nil {
+		d.Spec.Template.Annotations = make(map[string]string)
+	}
+
+	d.Spec.Template.Annotations[constants.ReloaderRolloutKey] = time.Now().Format(time.RFC3339)
+	if err := r.client.Update(ctx, d); err != nil {
+		return err
+	}
+
+	r.recorder.Eventf(d, corev1.EventTypeNormal, "Reloaded", "Deployment %s reloaded", d.Name)
+	r.log.Info("d reloaded", "Deployment", d.Name)
+
+	return nil
+}
+
+func (r *SecretController) reloadStatefulSet(ctx context.Context, ss *appsv1.StatefulSet) error {
+	if ss.Spec.Template.Annotations == nil {
+		ss.Spec.Template.Annotations = make(map[string]string)
+	}
+
+	ss.Spec.Template.Annotations[constants.ReloaderRolloutKey] = time.Now().Format(time.RFC3339)
+	if err := r.client.Update(ctx, ss); err != nil {
+		return err
+	}
+
+	r.recorder.Eventf(ss, corev1.EventTypeNormal, "Reloaded", "StatefulSet %s reloaded", ss.Name)
+	r.log.Info("StatefulSet reloaded", "StatefulSet", ss.Name)
+
+	return nil
+}
+
+func (r *SecretController) reloadDaemonSet(ctx context.Context, ds *appsv1.DaemonSet) error {
+	if ds.Spec.Template.Annotations == nil {
+		ds.Spec.Template.Annotations = make(map[string]string)
+	}
+
+	ds.Spec.Template.Annotations[constants.ReloaderRolloutKey] = time.Now().Format(time.RFC3339)
+	if err := r.client.Update(ctx, ds); err != nil {
+		return err
+	}
+
+	r.recorder.Eventf(ds, corev1.EventTypeNormal, "Reloaded", "DaemonSet %s reloaded", ds.Name)
+	r.log.Info("DaemonSet reloaded", "DaemonSet", ds.Name)
+
+	return nil
 }
